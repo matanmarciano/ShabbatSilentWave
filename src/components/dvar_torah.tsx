b2985a49a09fd196f243ed6e6ad4f2710512fb8e
Matan Marciano <mmarciano@seatgeek.com>
2024-02-04 12:56:32 +0200

    Fixes

--- src/components/dvar_torah.tsx
+++ src/components/dvar_torah.tsx
@@ -31,9 +31,16 @@ const DvarTorah: React.FC<Props> = ({parasha}) => {
                             fetch(`https://corsproxy.io/?${matches[0]}`)
                                 .then(response => response.text())
                                 .then(sourceCode => {
+                                    console.log(sourceCode)
 
-                                    const pTagRegex = /<p\s+class=ArticleText[^>]*>(.*?)<\/p>/g;
-                                    const matches = Array.from(sourceCode.matchAll(pTagRegex), match => match[0]);
+                                    var pTagRegex = /<p\s+class=ArticleText[^>]*>(.*?)<\/p>/g;
+                                    var matches = Array.from(sourceCode.matchAll(pTagRegex), match => match[0]);
+
+                                    if(matches.length == 0) {
+                                        pTagRegex = /<p\s+class="ArticleText"[^>]*>(.*?)<\/p>/g;
+                                        matches = Array.from(sourceCode.matchAll(pTagRegex), match => match[0]);
+                                    }
+
                                     const resultString = matches.map(tag => tag.replace(/<p[^>]*>/, '').replace(/<\/p>/, '')).join('\n');
 
                                     setDvarTorah(resultString)
